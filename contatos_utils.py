351bd03b0c108afee712ca134a9261338df0a599
Jonathan <jonathanjacobovski@alunos.utfpr.edu.br>
2022-09-27 11:26:51 -0300

    CSV

--- contatos_utils.py
+++ contatos_utils.py
@@ -1,4 +1,4 @@
-import csv
+import csv, pickle, json
 from contato import Contato
 
 def csv_para_contatos(caminho, encoding='latin_1'):
@@ -14,3 +14,39 @@ def csv_para_contatos(caminho, encoding='latin_1'):
             contatos.append(contato)
 
     return  contatos
+
+def contatos_para_pickles(contato, caminho):
+    with open(caminho, mode='wb') as arquivo:
+        pickle.dump(contato, arquivo)
+
+def pickle_para_contatos(caminho):
+    with open(caminho, mode='rb') as arquivo:
+        contatos = pickle.load(arquivo)
+
+    return contatos
+
+def contatos_para_json(contatos, caminho):
+    with open(caminho, mode='w') as arquivo:
+        json.dump(contatos, arquivo, default=_contato_para_json)
+
+def _contato_para_json(contato):
+    return contato.__dict__
+
+def json_para_contatos(caminho):
+    contatos = []
+
+    with open(caminho) as arquivo:
+        contatos_json = json.load(arquivo)
+
+        for contato in contatos_json:
+            c = Contato(**contato)
+            contatos.append(c)
+
+    return contatos
+
+
+
+
+
+
+
--- principal.py
+++ principal.py
@@ -1,9 +1,16 @@
 import contatos_utils
 
 try:
-   contatos = contatos_utils.csv_para_contatos('dados/contatos.csv')
+    contatos = contatos_utils.csv_para_contatos('dados/contatos.csv')
 
-   for contato in contatos:
+   # contatos_utils.contatos_para_pickles(contatos, 'dados/contatos.pickles')
+   # contatos = contatos_utils.pickle_para_contatos('dados/contatos.pickles')
+
+    contatos_utils.contatos_para_json(contatos, 'dados/contatos.json')
+    contatos = contatos_utils.json_para_contatos('dados/contatos.json')
+
+
+    for contato in contatos:
         print(f'{contato.id} - {contato.nome} - {contato.email}')
 except FileNotFoundError:
     print("Arquivo não encontrado")
